1a0e73657712e8394a5592ee2a92edbbbf693132
Lang <yianrita@gmail.com>
2025-03-15 13:56:10 +0000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [tailwindcss()],
-  base: "/eddies-home-maintenance",
+  base: "/eddies-home-maintenance/",
 });
